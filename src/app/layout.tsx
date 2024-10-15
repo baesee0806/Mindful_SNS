7c275707a78f22b0466c323975a95710d0dce8dc
FE_Wani <baesee@kakao.com>
2024-10-15 14:46:09 +0900

    Chore: esLint,prettier 설정
    - eslint: --save-dev eslint-config-prettier
    - .prettierrc 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,19 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next';
+import './globals.css';
 
 export const metadata: Metadata = {
-  title: "Mindful SNS",
-  description: "바른 SNS 서비스",
+	title: 'Mindful SNS',
+	description: '바른 SNS 서비스',
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body>{children}</body>
+		</html>
+	);
 }
