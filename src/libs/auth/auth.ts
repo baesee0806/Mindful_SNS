4c926ba70f101c2498a54374111f6de891c4f08a
FE_Wani <baesee@kakao.com>
2024-10-22 08:04:43 +0900

    Feat: firebase login & register & logout
    - auth.ts 파일에 각각의 함수를 만들었습니다.
    -  login: cookies를 이용하여 uid를 저장하여 로그인 여부를 알수있게 하였습니다.
    - logout: uid를 삭제하고 로그인 페이지로 이동하게 하였습니다.
    - register: 회원 가입을 하게 만들었고 유저를 저장하고 바로 홈으로 가게 하였습니다.
    
    Relate to: #3

---
+++ src/libs/auth/auth.ts
@@ -0,0 +1,52 @@
+import { auth } from '../firebase/firebaseClient';
+import {
+	signInWithEmailAndPassword,
+	createUserWithEmailAndPassword,
+	signOut,
+	// onAuthStateChanged,
+} from 'firebase/auth';
+import { cookies } from 'next/headers';
+
+// 쿠키 설정
+export function setCookie(name: string, value: string, maxAge: number) {
+	cookies().set(name, value, { httpOnly: true, secure: true, maxAge });
+}
+
+// 로그인 함수
+export async function login(email: string, password: string) {
+	try {
+		const userCredential = await signInWithEmailAndPassword(
+			auth,
+			email,
+			password,
+		);
+		console.log('Login success:', userCredential.user);
+		setCookie('uid', userCredential.user.uid, 60 * 60 * 24 * 7);
+		return userCredential.user;
+	} catch (error) {
+		console.error('Login error:', error);
+		throw error;
+	}
+}
+
+// 로그아웃 함수
+export async function logout() {
+	try {
+		await signOut(auth);
+		cookies().delete('uid');
+	} catch (error) {
+		console.error('Logout error:', error);
+		throw error;
+	}
+}
+
+// 회원가입 함수
+export async function signup(email: string, password: string) {
+	const userCredential = await createUserWithEmailAndPassword(
+		auth,
+		email,
+		password,
+	);
+	setCookie('uid', userCredential.user.uid, 60 * 60 * 24 * 7);
+	return userCredential.user;
+}
