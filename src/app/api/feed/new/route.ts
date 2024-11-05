360d8879e003af16e35ae67178d04eb4db127a4d
FE_Wani <baesee@kakao.com>
2024-11-05 20:53:20 +0900

    Feat: add feed를 위한 route 생성 및 infinity scroll에서 사용하는 GET route를 infinite경로로 변경
    - 새로운 피드 생성을 위한 POST 요청 route를 생성 (/api/feed/new 경로)
    - 단일 피드를 받아오는 것이 아니라서 restfull 하게 볼수있게 infinite로 여러 데이터 받게끔 경로 변경
    
    Relate to: #14

---
+++ src/app/api/feed/new/route.ts
@@ -0,0 +1,69 @@
+import { db, storage } from '@/libs/firebase/firebaseClient';
+import { addDoc, collection, doc, setDoc, Timestamp } from 'firebase/firestore';
+import {
+	getDownloadURL,
+	ref,
+	uploadBytes,
+	UploadMetadata,
+} from 'firebase/storage';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(req: NextRequest) {
+	const formData = await req.formData();
+	const feed_id = formData.get('feed_id') as string;
+	const title = formData.get('title') as string;
+	const content = formData.get('content') as string;
+	const user_id = formData.get('user_id') as string;
+	const img_adress = formData.get('img_adress') as File | Blob | string;
+	try {
+		// 이미지 storage 업로드
+		const feedImage = img_adress;
+		if (!(feedImage instanceof File || feedImage instanceof Blob)) {
+			throw new Error('Invalid image file provided');
+		}
+		const storageRef = ref(storage, `images/${feed_id}`);
+		const metadata: UploadMetadata = {
+			contentType: feedImage.type || 'image/jpeg',
+		};
+		await uploadBytes(storageRef, feedImage, metadata);
+		const imgUrl = await getDownloadURL(storageRef);
+
+		// feeds collection에 추가
+		const feedRef = doc(db, 'feeds', feed_id);
+		await setDoc(feedRef, {
+			feed_id,
+			title,
+			content,
+			user_id,
+			img_adress: imgUrl,
+			created_at: Timestamp.now(),
+			updated_at: Timestamp.now(),
+			like_count: 0,
+			comment_count: 0,
+		}).catch((error) => {
+			console.error('Error adding feed:', error);
+			return NextResponse.json(
+				{ error: 'Failed to add feed' },
+				{ status: 500 },
+			);
+		});
+
+		// feed_img collection에 추가
+		const feedImgRef = collection(db, 'feed_img');
+		await addDoc(feedImgRef, {
+			feed_id,
+			img_adress: imgUrl,
+			create_at: Timestamp.now(),
+			update_at: Timestamp.now(),
+		}).catch((error) => {
+			console.error('Error adding feed_img:', error);
+			return NextResponse.json(
+				{ error: 'Failed to add feed_img' },
+				{ status: 500 },
+			);
+		});
+	} catch (error) {
+		console.error('Error adding feed:', error);
+		return NextResponse.json({ error: 'Failed to add feed' }, { status: 500 });
+	}
+}
